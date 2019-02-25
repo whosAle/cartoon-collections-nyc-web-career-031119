3ebcd00e4dd0ad8cf05f851ade14285acfe05b3c
A Belgrave <abelgrave95@gmail.com>
2019-02-25 18:22:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet (planeteer_calls)# code an argument here
   planeteer_calls.collect { |call| call.capitalize << "!"  }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)# code an argument here
+  planeteer_calls.any? { |call| call.length > 4  }
 end
 
 def find_the_cheese# code an argument here
