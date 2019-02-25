fe7a09794596f9f76855a12c1edc53feaf8224fc
A Belgrave <abelgrave95@gmail.com>
2019-02-25 18:05:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarves.each_with_index { |dwarf, i| puts "#{i} #{dwarf}" }
 end
 
 def summon_captain_planet# code an argument here
